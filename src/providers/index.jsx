5953acfc336229c77bf6e93fbf3f69b9ff27be77
RojanSr <rairojan1116@gmail.com>
2023-08-08 14:29:41 +0545

    fix: fetch on mount issue

--- src/providers/index.jsx
+++ src/providers/index.jsx
@@ -3,7 +3,17 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { theme } from "../theme";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: true,
+      staleTime: 30 * 1000,
+      //   staleTime: 1000 * 60 * 5 // 5 minutes
+    },
+  },
+});
 
 const Provider = ({ children }) => {
   return (
